469561c62ad591f30ae05ab91618277e687e4216
Praveen Sharma <praying.sharma@gmail.com>
2025-08-11 16:29:13 +0530

    Navbar

--- Client/src/pages/student/Home.jsx
+++ Client/src/pages/student/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "../../components/students/Navbar";
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Navbar />
     </div>
   );
 }
