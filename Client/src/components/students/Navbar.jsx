f38d6aa028fd1db4d394e776f026f2ec1c49acc8
Praveen Sharma <praying.sharma@gmail.com>
2025-08-12 10:55:17 +0530

    SignIn

--- Client/src/components/students/Navbar.jsx
+++ Client/src/components/students/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 function Navbar() {
   const isCourseListPage = location.pathname.includes("/course-list");
+
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
   return (
     <div
       className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4  ${
@@ -19,9 +23,16 @@ function Navbar() {
           <button>Become Educator</button>|{" "}
           <Link to="/my-enrollments">My Enrollments</Link>
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-          Create Account
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+          >
+            Create Account
+          </button>
+        )}
       </div>
       {/* For Phone Screen  */}
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
