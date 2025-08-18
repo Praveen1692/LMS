fd83f4bbe6159e3f3f64efaef80e93c32c88d837
Praveen Sharma <praying.sharma@gmail.com>
2025-08-18 17:03:58 +0530

    openSections:

--- Client/src/pages/student/CourseDetails.jsx
+++ Client/src/pages/student/CourseDetails.jsx
@@ -9,6 +9,14 @@ function CourseDetails() {
   const { id } = useParams();
 
   const [courseData, setCourseData] = useState(null);
+  const [openSections, setOptionSections] = useState({});
+
+  const toggleSection = (index) => {
+    setOptionSections((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
 
   const {
     allCourses,
@@ -88,7 +96,10 @@ function CourseDetails() {
                   key={index}
                   className="border border-gray-300 bg-white mb-2 rounded"
                 >
-                  <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                  <div
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                    onClick={() => toggleSection(index)}
+                  >
                     <div className="flex items-center gap-2">
                       <img src={assets.down_arrow_icon} alt="arrow_icon" />
                       <p className="font-medium md:text-base text-sm">
@@ -101,7 +112,9 @@ function CourseDetails() {
                     </p>
                   </div>
 
-                  <div className="overflow-hidden transition-all duration-300 max-h-96">
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' :'max-h-0'} `}
+                  >
                     <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
                       {chapter.chapterContent.map((lecture, index) => (
                         <li key={index} className="flex items-start gap-2 py-1">
@@ -113,7 +126,11 @@ function CourseDetails() {
                           <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
                             <p>{lecture.lectureTitle}</p>
                             <div className="flex gap-2">
-                              {lecture.isPreviewFree && <p>Preview</p>}
+                              {lecture.isPreviewFree && (
+                                <p className="text-blue-500 cursor-pointer">
+                                  Preview
+                                </p>
+                              )}
                               <p>
                                 {humanizeDuration(
                                   lecture.lectureDuration * 60 * 1000,
